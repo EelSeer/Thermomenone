c93b7e4dd78d0fcd11ae6679fea98626aeccd9b7
Tom Rees-Lee <trees-lee@fairfaxmedia.com.au>
2015-06-21 14:36:55 +1000

    Initial Commit

---
+++ Thermomenone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Thermomenone
+//
+//  Created by Tom Rees-Lee on 21/06/2015.
+//  Copyright (c) 2015 Hovercraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Thermomenone/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Thermomenone
+//
+//  Created by Tom Rees-Lee on 21/06/2015.
+//  Copyright (c) 2015 Hovercraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
