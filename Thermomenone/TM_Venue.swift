0d920ce3afee56a2b0ca6ff668b04911cb89e7e2
Tom Rees-Lee <trees-lee@fairfaxmedia.com.au>
2015-06-21 18:52:35 +1000

    Added updateVenue method to TM_Venue as an extension.
    Needs a bit of work on checking for correctness in dict values around wind and humidity, since we’re converting from strings to numbers. Generally if we get something malformed we’ll want to set it to an unknown state but keep processing the dictionary.

--- Thermomenone/TM_Venue.swift
+++ Thermomenone/TM_Venue.swift
@@ -26,26 +26,6 @@ public enum TM_CountryKey: String {
     case countryName = "_name"
 }
 
-@objc public enum TM_CompassDirection: Int {
-    case None = -1
-    case North
-    case NNE
-    case NE
-    case ENE
-    case East
-    case ESE
-    case SE
-    case SSE
-    case South
-    case SSW
-    case SW
-    case WSE
-    case West
-    case WNW
-    case NW
-    case NNW
-}
-
 public class TM_Venue : NSObject {
     public let venueName: String
     
@@ -58,7 +38,7 @@ public class TM_Venue : NSObject {
     public var countryID: NSNumber?
     public var weatherCondition: String?
     public var weatherConditionIcon: String?
-    public var weatherWindDirection: TM_CompassDirection = TM_CompassDirection.None
+    public var weatherWindDirection: String?
     public var weatherWindSpeed: NSNumber?
     public var weatherHumidity: NSNumber?
     public var weatherTemp: NSNumber?
@@ -67,7 +47,93 @@ public class TM_Venue : NSObject {
 }
 
 extension TM_Venue {
+    
     public func updateVenue(venueDictionary: Dictionary<String, AnyObject>) {
+        //check that venue object is valid. we don't want to update at all if we got the wrong venue here.
+        if let v: String = TM_Venue.manifestValueForKey(venueDictionary, key: TM_Venue_ManifestKey.venueName) {
+            if v != self.venueName {
+                return
+            }
+        }
         
+        if let v: NSNumber = TM_Venue.manifestValueForKey(venueDictionary, key: TM_Venue_ManifestKey.weatherLastUpdated) {
+            let lastUpdateDate = NSDate(timeIntervalSince1970: v.doubleValue)
+            if self.lastUpdated == nil || lastUpdateDate.compare(self.lastUpdated!) == NSComparisonResult.OrderedAscending {
+                self.lastUpdated = lastUpdateDate
+            } else {
+                return
+            }
+        } else {
+            self.lastUpdated = nil
+        }
+
+        if let v: String = TM_Venue.manifestValueForKey(venueDictionary, key: TM_Venue_ManifestKey.weatherCondition) {
+            self.weatherCondition = v
+        } else {
+            self.weatherCondition = nil
+        }
+        
+        if let v: String = TM_Venue.manifestValueForKey(venueDictionary, key: TM_Venue_ManifestKey.weatherConditionIcon) {
+            self.weatherConditionIcon = v
+        } else {
+            self.weatherConditionIcon = nil
+        }
+        
+        if let v: String = TM_Venue.manifestValueForKey(venueDictionary, key: TM_Venue_ManifestKey.weatherWind) {
+            let windTokens = v.componentsSeparatedByString(" ")
+            var speedVal : Double? = nil
+            var speedDir : String? = nil
+            if count(windTokens) == 4 {
+                speedDir = windTokens[1]
+                let speed = windTokens[3]
+                let kphrange = speed.rangeOfString("kph")
+                speedVal = (speed.substringToIndex(kphrange!.startIndex) as NSString).doubleValue
+            }
+            
+            if (speedVal != nil && speedDir != nil) {
+                self.weatherWindSpeed = speedVal!
+                self.weatherWindDirection = speedDir!
+            } else {
+                self.weatherWindDirection = nil
+                self.weatherWindSpeed = nil
+            }
+        } else {
+            self.weatherWindDirection = nil
+            self.weatherWindSpeed = nil
+        }
+        
+        if let v: String = TM_Venue.manifestValueForKey(venueDictionary, key: TM_Venue_ManifestKey.weatherHumidity) {
+            let humidityVal = v.stringByReplacingOccurrencesOfString("Humidity: ", withString: "").stringByReplacingOccurrencesOfString("%", withString: "")
+            let doubleVal = (humidityVal as NSString).doubleValue
+            if (doubleVal <= 100 && doubleVal >= 0) {
+                self.weatherHumidity = doubleVal
+            } else {
+                self.weatherHumidity = nil
+            }
+        } else {
+            self.weatherHumidity = nil
+        }
+        
+        if let v: Int = TM_Venue.manifestValueForKey(venueDictionary, key: TM_Venue_ManifestKey.weatherTemp) {
+            self.weatherTemp = v
+        } else {
+            self.weatherTemp = nil;
+        }
+        
+        if let v: Int = TM_Venue.manifestValueForKey(venueDictionary, key: TM_Venue_ManifestKey.weatherFeelsLike) {
+            self.weatherFeelsLike = v
+        } else {
+            self.weatherFeelsLike = nil
+        }
+    }
+    
+    private class func manifestValueForKey<T>(dictionary: Dictionary<String, AnyObject>, key: TM_Venue_ManifestKey) -> T? {
+        let k = key.rawValue
+        if let v = dictionary[k] as? T? {
+            return v
+        }
+        // If the value is not nil, then assert it.
+        assert([k].self == nil, "Could not find value for \(k)")
+        return nil
     }
 }
